9ac9c5f388219bc45f54630ae8fc104be490e400
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 02:02:36 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
+    session[:username] = params[:username]
+    redirect_to '/'
   end
 
   def destroy

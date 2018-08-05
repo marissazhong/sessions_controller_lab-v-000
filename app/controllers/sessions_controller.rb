2683cea732de5657c7a879297113d233918508e4
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 02:12:26 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+
+  def hello
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  end
+
+end
